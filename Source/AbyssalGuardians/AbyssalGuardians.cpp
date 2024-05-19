bbecb07b5b656d4a83b412dd7490b5f4ffba266f
Ivan Gomez <ivangqj@gmail.com>
2024-05-19 17:43:05 -0600

    Project Updated

--- Source/AbyssalGuardians/AbyssalGuardians.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AbyssalGuardians.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AbyssalGuardians, "AbyssalGuardians" );
